066439e0e08439897835c93fe2f7ec34db7158d4
suckwonlee <lsw980912@gmail.com>
2024-09-12 13:10:44 +0900

    0912 클래스

---
+++ src/chp07/test4/Cube.java
@@ -0,0 +1,12 @@
+package chp07.test4;
+
+
+public class Cube {
+    int length=5;
+    Cube c=new Cube();
+    int Volume(){
+        int getvolume=(int)Math.pow(length,3);
+        return getvolume;
+    }
+
+}
