b96da3f0fdd6fe9ba9f3346e8898c052d675a47a
leesuckwon <lsw980912@gmail.com>
2024-10-17 09:45:47 +0900

    업캐스팅: 실습 10-4(어레이리스트)

--- src/chp10/ex10_4/MainShape.java
+++ src/chp10/ex10_4/MainShape.java
@@ -1,5 +1,7 @@
 package chp10.ex10_4;
 
+import java.util.ArrayList;
+
 public class MainShape {
     public static void main(String[] args) {
         Square s = new Square();
@@ -10,10 +12,13 @@ public class MainShape {
         c.name="원";
 
 
-        Shape[] shapes = {s,t,c};
+        ArrayList<Shape> shapes=new ArrayList<>();
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
 
-        for (int i=0;i<shapes.length;i++){
-            System.out.printf("인덱스 번호 %d번의 도형이름 : %s\n",i,shapes[i].name);
+        for (int i=0;i<shapes.size();i++){
+            System.out.printf("인덱스 번호 %d번의 도형이름 : %s\n",i,shapes.get(i).name);
 
         }
     }
